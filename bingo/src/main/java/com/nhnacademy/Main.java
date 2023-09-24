65382f302c77a9693f84a4f8c913f1d4c5d44620
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2023-09-24 15:44:49 +0900

    빙고판 만들기

--- bingo/src/main/java/com/nhnacademy/Main.java
+++ bingo/src/main/java/com/nhnacademy/Main.java
@@ -1,8 +1,9 @@
-package main.java.com.nhnacademy;
+package com.nhnacademy;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bingo");
-
+        
+        
     }
 }
