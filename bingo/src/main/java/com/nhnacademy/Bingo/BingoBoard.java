b4ffc340a1be051d8c43621a6d40cc5742627eff
shkcreate0518 <shkcreate0518@gmail.com>
2023-09-24 18:23:30 +0900

    수정

--- bingo/src/main/java/com/nhnacademy/Bingo/BingoBoard.java
+++ bingo/src/main/java/com/nhnacademy/Bingo/BingoBoard.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.Bingo;
 
-
 import java.util.Scanner;
 
 public class BingoBoard {
@@ -11,7 +10,6 @@ public class BingoBoard {
     private static String twoPlayer = "X";
 
     static Scanner scanner = new Scanner(System.in);
-
 
     // n*n크기의 빙고판 생성
     public BingoBoard(int size) {
@@ -50,7 +48,6 @@ public class BingoBoard {
         }
     }
 
-
     public static void pickNumber() {
         for (int a = 1; a <= (size * size); a++) {
             int num = scanner.nextInt();
@@ -68,7 +65,6 @@ public class BingoBoard {
             winPlayer();
         }
     }
-
 
     public static void winPlayer() {
         rowBingoCheck();
@@ -119,28 +115,6 @@ public class BingoBoard {
             }
         }
         return null;
-
-
-    public static String leftDiagonal() {
-        int oneCount = 0;
-        int twoCount = 0;
-        for (int i = 0; i < 5; i++) {
-            if (bingo[i][i] == 0) {
-                oneCount++;
-                if (oneCount == 5) {
-                    return onePlayer;
-                }
-            }
-            if (bingo[i][i] == 1) {
-                twoCount++;
-                if (twoCount == 5) {
-                    return twoPlayer;
-                }
-            }
-        }
-        return null;
-    }
-
 
     }
 
