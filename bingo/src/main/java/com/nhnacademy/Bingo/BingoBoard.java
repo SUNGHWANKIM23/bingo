63f6a24434bfeb13975dd4913e13e9c2746fe93e
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2023-09-25 00:35:11 +0900

    직접 입력해서 숫자 넣기 작성중

--- bingo/src/main/java/com/nhnacademy/Bingo/BingoBoard.java
+++ bingo/src/main/java/com/nhnacademy/Bingo/BingoBoard.java
@@ -1,5 +1,14 @@
 package com.nhnacademy.Bingo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class BingoBoard {
+    Socket socket;
+    
     //빙고판 초기화
     static int[][] bingo;
     static int size;
@@ -27,6 +36,40 @@ public class BingoBoard {
                 bingo[x][y] = tmp;
             }
         }
+    }
+
+    //직접 입력해서 숫자 넣기
+    public static void selectBingoNum(Socket socket) throws IOException{
+        
+        int maxNum = size*size;
+        Scanner scanner = new Scanner(new InputStreamReader(socket.getInputStream()));
+        boolean isDuplicate = false;
+        //socket.getInputStrean or System.in
+        for(int i = 0; i < size ; i++){
+            for(int j = 0; j < size; j++){
+                
+                while(!isDuplicate){
+                    System.out.println("빙고판 안에 넣을 숫자를 입력하세요.");  
+                    int selecNum = scanner.nextInt();
+                    
+                    //중복된 숫자인지 확인
+                    for(int k = 0; k < i; k++){
+                        for(int l = 0; l < j; l++){
+                            if(bingo[k][j] == selecNum){
+                                isDuplicate = true;
+                                System.out.println("중복된 숫자입니다. 다른 숫자를 입력하세요.");
+                                break;
+                            }
+                        }
+                    }
+                    if(!isDuplicate){
+                        bingo[i][j] = selectNum;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("빙고판 안의 숫자가 모두 채워졌습니다.");
     }
 
     //빙고판 출력
